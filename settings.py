e25bed05cdd6879635b522d6949ba74c5952b39f
ethanbg2 <ethanbg2@illinois.edu>
2022-02-07 16:27:04 -0600

    settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from dotenv import find_dotenv
 import os
-load_dotenv(find_dotenv("keys.env"))
+load_dotenv(find_dotenv(".env"))
 
 APIKEY = os.environ.get("APIKEY")
 APISECRETKEY = os.environ.get("APISECRETKEY")
