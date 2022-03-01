cbe5d06bbaa85d9ac28d921a4156135660455c2e
ethanbg2 <ethanbg2@illinois.edu>
2022-03-01 00:21:39 -0600

    created initial code to build features for graphs

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,6 +4,7 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import pandas as pd
+from graph_embed import GraphEmbed
 
 
 def main(proj_dir):
@@ -12,6 +13,14 @@ def main(proj_dir):
     """
     logger = logging.getLogger(__name__)
     logger.info('extracting features from graphs')
+
+    all_networks = pd.read_csv(os.path.join(proj_dir, "data", "raw", "all_networks.csv"))
+
+    GE = GraphEmbed("graph2vec")
+    GE.build_graphs(all_networks, 5)
+    GE.fit()
+    embedding_df = GE.get_embedding_df()
+    embedding_df.to_pickle(os.path.join(proj_dir, "data", "processed", "graph_embeddings.pkl"))
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
--- src/features/graph_embed.py
+++ src/features/graph_embed.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import numpy as np
 import networkx as nx
+import karateclub.graph_embedding as ge
 
 class GraphEmbed:
-    def __init__(self):
-        pass
-
+    models = {"feather": ge.FeatherGraph(), "graph2vec": ge.Graph2Vec()}
+    def __init__(self, type, params=None):
+        self.model = self.models[type]
+        if not params is None:
+            for k, v in params.items():
+                self.model.__dict__[k] = v
+    
     def build_graphs(self, graph_df, min_edges):
-        graphs = []
+        self.graphs = []
+        self.ids = []
         for id in graph_df.id.unique():
             network = graph_df.loc[graph_df.id == id]
             if (len(network) < min_edges):
@@ -16,7 +22,22 @@ class GraphEmbed:
                 edges = list(zip(network.from_user_id, network.to_user_id))
                 g = nx.DiGraph()
                 g.add_edges_from(edges)
-                graphs.append(g)
+                relabel_g = nx.convert_node_labels_to_integers(g)
+                # append data
+                self.graphs.append(relabel_g)
+                self.ids.append(id)
 
-        return graphs
+    def get_graphs(self):
+        return self.graphs
+
+    def fit(self):
+        self.model.fit(self.graphs)
+    
+    def get_embedding(self):
+        return self.model.get_embedding()
+    
+    def get_embedding_df(self):
+        data = list(zip(self.ids, self.get_embedding()))
+        return pd.DataFrame(data, columns=["article_id", "graph_embedding"])
+
 
